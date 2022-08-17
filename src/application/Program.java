2cf311c7e81399092fdd929764f6d180f0075972
PauloTakamori <lino.x@hotmail.com>
2022-08-17 09:59:42 -0300

    Implementando update

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ======");
+		seller = sellerDao.findById(1); //carregando os dados do vendedor id 1 na variavel seller.
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
+		
 	}
 
 }
